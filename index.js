2cce80065a1e37e9e2704debd77cd32673d58e8f
rona chen <rona.chen@hotmail.com>
2023-05-27 19:38:51 -0400

    added shapes class with trianle, circle, and square extending from it in the shapes.js file and exported for use

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const questions = require('./lib/questions')
+const questions = require('./lib/questions');
+const { Circle, Square, Triangle } = require('./lib/shapes');
 
 inquirer.prompt(questions).then((data) => {
     fs.writeFile('logo.svg', generateLogo(data), (err) => 
