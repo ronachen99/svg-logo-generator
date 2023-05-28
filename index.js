4893b6dfc759bb9308babdb23a5571136867f9dd
rona chen <rona.chen@hotmail.com>
2023-05-28 12:43:52 -0400

    incorporated aync await function, made sure the child classes inherited the parent shapes class using super in the shapes.js and added code to the genertatelogo.js so the code  generates the lopg

--- index.js
+++ index.js
@@ -2,9 +2,36 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const questions = require('./lib/questions');
 const { Circle, Square, Triangle } = require('./lib/shapes');
+const Logo = require('./lib/generateLogo')
 
-inquirer.prompt(questions).then((data) => {
-    fs.writeFile('logo.svg', generateLogo(data), (err) => 
-        err ? console.log(err) : console.log('Generated logo.svg!')
-    );
-});
+const getUserInput = async () => {
+    try {
+        const data = await inquirer.prompt(questions);
+
+        if (data.text.length > 3 || data.text.length === 0) {
+           return console.log('Logo text should be more than 0 and less than 3 characters in length.');
+        } 
+        
+        const logoShape = setShape(data.shape);
+        logoShape.setColour(data.shapeColour);
+        const logo = new Logo(data.text, data.textColour, logoShape);
+
+        await fs.promises.writeFile('examples/logo.svg', logo.renderSVG());
+        console.log('Successfully generated a logo.svg in the examples folder.');
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function setShape(shape) {
+    switch (shape) {
+        case 'Circle':
+            return new Circle();
+        case 'Square':
+            return new Square();
+        case 'Triangle':
+            return new Triangle();
+    }
+}
+
+getUserInput();
