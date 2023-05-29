03d5c28c0de87b95926c546523033220f138c530
rona chen <rona.chen@hotmail.com>
2023-05-29 02:17:15 -0400

    changed the format of the shapes that is being created, added if statements for exception cases, and modified the shapes.test.js to match the expected output

--- index.js
+++ index.js
@@ -2,24 +2,34 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const questions = require('./lib/questions');
 const { Circle, Square, Triangle } = require('./lib/shapes');
-const Logo = require('./lib/generateLogo')
+const Logo = require('./lib/generateLogo');
+var validateColor = require("validate-color").default;
+
 
 const getUserInput = async () => {
     try {
         const data = await inquirer.prompt(questions);
 
-        if (data.text.length > 3 || data.text.length === 0) {
-           return console.log('Logo text should be more than 0 and less than 3 characters in length.');
-        } 
-        
+        if (data.text.length === 0 || data.text.length > 3) {
+            throw ('Invalid Text: Logo text should be more than 0 and less than 3 characters in length.');
+        };
+
+        if (!validateColor(data.textColour)){
+             throw ('Invalid Text Colour: File creation aborted.');
+        };
+
+        if (!validateColor(data.shapeColour)){
+             throw ('Invalid Shape Colour: File creation aborted.');
+        };
+
         const logoShape = setShape(data.shape);
-        logoShape.setColour(data.shapeColour);
+        logoShape.setColour(data.shapeColour); 
         const logo = new Logo(data.text, data.textColour, logoShape);
 
         await fs.promises.writeFile('examples/logo.svg', logo.renderSVG());
         console.log('Successfully generated a logo.svg in the examples folder.');
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
 
