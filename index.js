a18535612e89549aa0958cba1ab260eccd95fdea
rona chen <rona.chen@hotmail.com>
2023-05-29 21:08:38 -0400

    took out validate colour pkg, added default properties to question.js, took out if statements in the index.js file

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
+//==========================================================================================================================================//
+//--------------------------------------------------------Import Modules and Packages-------------------------------------------------------//
 const inquirer = require('inquirer');
+const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
+inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
+const ChalkPipe = require('inquirer-chalk-pipe');
+inquirer.registerPrompt('chalk-pipe', ChalkPipe);
 const fs = require('fs');
 const questions = require('./lib/questions');
 const { Circle, Square, Triangle } = require('./lib/shapes');
 const Logo = require('./lib/generateLogo');
-var validateColor = require("validate-color").default;
 
-
+//==========================================================================================================================================//
+//--------------------------------------------------Get User Input: set values for the logo-------------------------------------------------//
 const getUserInput = async () => {
     try {
         const data = await inquirer.prompt(questions);
-
-        if (data.text.length === 0 || data.text.length > 3) {
-            throw new Error('Invalid Text: Logo text should be more than 0 and less than 3 characters in length.');
-        };
-
-        if (!validateColor(data.textColour)) {
-            throw new Error('Invalid Text Colour: File creation aborted.');
-        };
-
-        if (!validateColor(data.shapeColour)) {
-            throw new Error('Invalid Shape Colour: File creation aborted.');
-        };
-
         const logoShape = setShape(data.shape);
         logoShape.setColour(data.shapeColour);
         const logo = new Logo(data.text, data.textColour, logoShape);
@@ -33,6 +26,8 @@ const getUserInput = async () => {
     }
 }
 
+//==========================================================================================================================================//
+//----------------------------------------Set Shape: change the shape of the logo based on user input---------------------------------------//
 function setShape(shape) {
     switch (shape) {
         case 'Circle':
